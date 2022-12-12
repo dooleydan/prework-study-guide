8d14340b1ef0e22b6fe2f4825b1c8401308cf4c2
dooleydan <git@github.com>
2022-12-11 21:55:48 -0600

    yes

--- assets/script.js
+++ assets/script.js
@@ -8,13 +8,13 @@ function listTopics() {
 
 
 function selectTopic() {
- if (topics[0] === 'HTML')  {
+ if (randomTopic === 'HTML')  {
      console.log("Let's study HTML!");
- } else if (topics[0] === 'CSS') {
+ } else if (randomTopic === 'CSS') {
      console.log("Let's study CSS!");
- } else if (topics[0] === 'Git') {
+ } else if (randomTopic === 'Git') {
      console.log("Let's study Git!");
- } else if (topics[0] === 'JavaScript') {
+ } else if (randomTopic === 'JavaScript') {
      console.log("Let's study JavaScript!");
  } else {
      console.log('Please try again');
