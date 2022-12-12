389ca5311877c34fcad7ef9fd8f65a6eade5064e
dooleydan <git@github.com>
2022-12-11 21:00:56 -0600

    pt. 2

--- assets/script.js
+++ assets/script.js
@@ -4,16 +4,18 @@ function listTopics() {
     console.log(topics[x])
  }
 }
+
+
 function selectTopic() {
-    if (topic === 'HTML')  {
-        console.log("Let's study HTML!");
-    }else if (topic === 'CSS') {
-        console.log("Let's study CSS!");
-    } else if (topic === 'Git') {
-        console.log("Let's study Git!");
-    } else if (topic === 'JavaScript') {
-        console.log("Let's study JavaScript!");
-    } else {
-        console.log('Please try again');
-    }
+ if (topic === 'HTML')  {
+     console.log("Let's study HTML!");
+ }else if (topic === 'CSS') {
+     console.log("Let's study CSS!");
+ } else if (topic === 'Git') {
+     console.log("Let's study Git!");
+ } else if (topic === 'JavaScript') {
+     console.log("Let's study JavaScript!");
+ } else {
+     console.log('Please try again');
+ }
 }
