fca505454173e68159f72a1c55593db0e42ae7e0
dooleydan <git@github.com>
2022-12-11 21:42:44 -0600

    3.4.4 pt. 2

--- assets/script.js
+++ assets/script.js
@@ -21,4 +21,7 @@ function selectTopic() {
  }
 }
 
-listTopics()
+console.log('Here are the topics we learned through Prework:');
+listTopics();
+console.log('Which topic should we study first?');
+selectTopic();
